63ef554791b19e202dcbbaae8d3191131dbf95cf
cxt181 <3452708074@qq.com>
2022-09-21 15:50:51 +0800

    这是第二次提交

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,8 +1,13 @@
 <template>
     <div>布局页</div>
     <router-view/>
-    <div>123</div>
-</template>
+    <van-tabbar v-model="active">
+        <van-tabbar-item icon="home-o">标签</van-tabbar-item>
+        <van-tabbar-item icon="search">标签</van-tabbar-item>
+        <van-tabbar-item icon="friends-o">标签</van-tabbar-item>
+        <van-tabbar-item icon="setting-o">标签</van-tabbar-item>
+    </van-tabbar>
+   </template>
 
 <script >
     import { Tabbar, TabbarItem } from 'vant';
