540445db5652ee82d0f512474a8b6678df6fa67f
cxt181 <3452708074@qq.com>
2022-09-21 16:52:41 +0800

    这是第四次提交

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -2,10 +2,10 @@
     <div>布局页</div>
     <router-view/>
     <van-tabbar v-model="active">
-        <van-tabbar-item icon="home-o">1</van-tabbar-item>
-        <van-tabbar-item icon="search">2</van-tabbar-item>
+        <van-tabbar-item icon="home-o">首页</van-tabbar-item>
+        <van-tabbar-item icon="search">拼团</van-tabbar-item>
         <van-tabbar-item icon="friends-o">3</van-tabbar-item>
-        <van-tabbar-item icon="setting-o">4</van-tabbar-item>
+        <van-tabbar-item icon="setting-o">4cxt</van-tabbar-item>
     </van-tabbar>
    </template>
 
