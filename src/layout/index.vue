700366e8ab99b1f1c70eee7992582e176a1bd76c
cxt181 <3452708074@qq.com>
2022-09-21 15:58:41 +0800

    这是第三次提交

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -2,10 +2,10 @@
     <div>布局页</div>
     <router-view/>
     <van-tabbar v-model="active">
-        <van-tabbar-item icon="home-o">标签</van-tabbar-item>
-        <van-tabbar-item icon="search">标签</van-tabbar-item>
-        <van-tabbar-item icon="friends-o">标签</van-tabbar-item>
-        <van-tabbar-item icon="setting-o">标签</van-tabbar-item>
+        <van-tabbar-item icon="home-o">1</van-tabbar-item>
+        <van-tabbar-item icon="search">2</van-tabbar-item>
+        <van-tabbar-item icon="friends-o">3</van-tabbar-item>
+        <van-tabbar-item icon="setting-o">4</van-tabbar-item>
     </van-tabbar>
    </template>
 
